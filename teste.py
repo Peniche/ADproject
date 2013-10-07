5e23afda8c4bfce2586200c4c48786459369cd66
Peniche <peniche.games@gmail.com>
2013-10-07 10:29:24 +0100

    Update and rename teste.c to teste.py

---
+++ teste.py
@@ -0,0 +1,64 @@
+1. num = int(raw_input('insira um numero'))
+    exp = 0
+    while 2**exp<num :
+       print 2**exp
+       exp += 1
+
+--------------------------------------------------------------
+2. num = int(raw_input('insira um numero'))
+
+    div = 0
+    while num%div = 0
+       div += 1
+   print consegue dividir em : , div
+
+------------------------------------------------------------------
+3.num = int(raw_input('insira um numero'))
+
+  soma = 0
+  div = 2
+
+  while div < num : 
+          if num %div == 0:
+          soma += div
+          div += 1
+         
+  print 'resultado', soma
+
+-----------------------------------------------------------------
+
+4. num = int(raw_input('insira um numero'))
+  
+  soma = 0
+  div = 2
+
+  while div < num : 
+          if num %div == 0:
+          soma += div
+          div += 1
+          print 'soma dos divisores proprios', soma
+  if num = soma + 1 : 
+          print 'eh perfeito'
+  else:
+          print ' nao eh perfeito'
+
+-----------------------------------------------------------------
+5.num = int(raw_input('Insira um numero'))
+
+ePrimo = True   
+div = 1
+while num < div and ePrimo : 
+         if  num % div == 0 : 
+             ePrimo = False
+         div += 1
+if ePrimo:
+         print 'o numero eh primo'
+else:
+         print 'o numero n eh primo'
+
+-----------------------------------------------------------------
+
+6.
+
+        
+      
