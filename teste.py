8a49dad4de4f4d6fdfc7bc19d38496137f635398
Peniche <peniche.games@gmail.com>
2013-10-14 10:27:03 +0100

    Update teste.py

--- teste.py
+++ teste.py
@@ -57,8 +57,94 @@ else:
          print 'o numero n eh primo'
 
 -----------------------------------------------------------------
+// folha 4
+1. a)
+num=int (raw_input('insira um numero'))
 
-6.
+div = 1
+soma = num
 
-        
+while num/2>= div:
+        if  num % div == 0
+            soma += div
+        div += 1
+print 'a soma dos disivsores eh' , soma
+-----------------------------------------------------------------------
+
+1. b) /* no ciclo for a parte do in range percorre todos os numeros menos o ultimo...
+ou seja de 0 a 10 so vai ate 9*/
+
+num = int(raw_input('insira um numero'))
+
+soma = num
+
+for div = 1 in range (1, num/2 + 1 )
+    num % div ==0
+    soma += div
+print 'A soma dos divisores eh', soma    
+
+------------------------------------------------------------
+
+3.
+
+num = int(raw_input('insira um numero'))
+// my way
+fact = 0
+
+for 0<div = num in range ( 0, num +1):
+    fact = num % div
+    print fact
+    div -= 1
+    
+    --------
+//prof way    
+fact = 1
+for mult in range (2, num+1):
+    fact *= mult
+print ' o factorial eh', fact    
+    
+
+-----------------------------------------------------------
+
+4.
+
+num = int(raw_input('insira um numero'))
       
+soma = 0
+
+for i in range (1, num+1, 2):
+    soma +=1
+print 'A soma dos impares eh', soma
+
+
+-----------------------------------------------------------
+
+5.
+
+num = float(raw_input('insira um numero'))
+
+
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+    
